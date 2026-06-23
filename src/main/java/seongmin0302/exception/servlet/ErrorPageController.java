840ccc3a5618404ece8cc78d83543a0db7431ebc
seongmin0302 <seongmin0302@naver.com>
2026-06-23 01:19:35 +0000

    ErrorPageController 생성

---
+++ src/main/java/seongmin0302/exception/servlet/ErrorPageController.java
@@ -0,0 +1,28 @@
+package seongmin0302.exception.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class ErrorPageController {
+
+    @RequestMapping("/error-page/404")
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
+        log.info("WebServerCustomizer의해서 ErrorPageController 호출되어 errorPage 404 보여주겠습니다.");
+        return "error-page/404";
+    }
+
+    @RequestMapping("/error-page/500")
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
+        log.info("WebServerCustomizer의해서 ErrorPageController 호출되어 errorPage 404 보여주겠습니다.");
+        return "error-page/500";
+    }
+}
+
+    
